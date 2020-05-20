2b332d29a5bd2e2ed2e6346d67abd63bd87a88f7
Matt Farncombe <matt@farncombe.co>
2020-05-20 15:13:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,9 @@ class Song
   def self.genre_count
     if @@genre_count.length > 0
       @@genre_count.each do |genre, count|
-        @@genre_count[@genre]=count += 1
+        puts genre
+        puts count
+        @@genre_count[genre]=count += 1
       end
     else
       @@genre_count[@genre] = 1
